d7f08dc729b71b09c61c27f5211f7764b22b3f94
tree <hhyjs2r@gmail.com>
2024-03-16 23:13:44 +0900

    feat: create Packages Model

---
+++ models/Package.ts
@@ -0,0 +1,29 @@
+export class Packages {
+  #downloads: number;
+  #start: string;
+  #end: string;
+  #packName: string;
+
+  constructor(downloads: number, start: string, end: string, packName: string) {
+    this.#downloads = downloads;
+    this.#start = start;
+    this.#end = end;
+    this.#packName = packName;
+  }
+
+  get downloads(): number {
+    return this.#downloads;
+  }
+
+  get start(): string {
+    return this.#start;
+  }
+
+  get end(): string {
+    return this.#end;
+  }
+
+  get packName(): string {
+    return this.#packName;
+  }
+}
