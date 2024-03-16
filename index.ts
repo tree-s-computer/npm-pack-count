123eae854a28d8af53fc7a61d972c4ee5819a673
tree <hhyjs2r@gmail.com>
2024-03-17 00:17:19 +0900

    refactor : change private

--- index.ts
+++ index.ts
@@ -6,15 +6,15 @@ import { OutputView } from "./views/OutputView";
 
 class DownloadTracker {
   packages: string[];
-  #weekNum: number;
-  #weekPacks: Packages[][] = [];
+  private weekNum: number;
+  private weekPacks: Packages[][] = [];
 
   constructor(packages, weekNum) {
     this.packages = packages;
-    this.#weekNum = weekNum;
+    this.weekNum = weekNum;
   }
 
-  async #getDownloadsForWeek(packageName: string, start: any, end: any) {
+  private async getDownloadsForWeek(packageName: string, start: any, end: any) {
     const url = `${URL}${start}:${end}/${packageName}`;
 
     try {
@@ -32,7 +32,7 @@ class DownloadTracker {
     }
   }
 
-  #setWeekData(startDate, endDate, i) {
+  private setWeekData(startDate, endDate, i) {
     // Calculate the start and end dates for each week (Yesterday to Yesterday)
     const start = new Date(startDate);
     start.setDate(startDate.getDate() - i * 7); // Go back i weeks
@@ -41,7 +41,7 @@ class DownloadTracker {
     return { start, end };
   }
 
-  #startSettingDate() {
+  private startSettingDate() {
     const today = new Date();
     const endDate = new Date(today);
     endDate.setDate(today.getDate() - 1); // Yesterday
@@ -51,14 +51,14 @@ class DownloadTracker {
     return { startDate, endDate };
   }
 
-  async #getWeeklyDownloadsFromYesterday(packageName: string) {
+  private async getWeeklyDownloadsFromYesterday(packageName: string) {
     const weekPack: Packages[] = [];
-    const { startDate, endDate } = this.#startSettingDate();
+    const { startDate, endDate } = this.startSettingDate();
 
-    for (let i = 0; i < this.#weekNum; i++) {
-      const { start, end } = this.#setWeekData(startDate, endDate, i);
+    for (let i = 0; i < this.weekNum; i++) {
+      const { start, end } = this.setWeekData(startDate, endDate, i);
 
-      const pack = await this.#getDownloadsForWeek(
+      const pack = await this.getDownloadsForWeek(
         packageName,
         formatDate(start),
         formatDate(end)
@@ -70,16 +70,16 @@ class DownloadTracker {
     return weekPack;
   }
 
-  async #getWeekPacks() {
+  private async getWeekPacks() {
     for (const packageName of this.packages) {
-      const weekPack = await this.#getWeeklyDownloadsFromYesterday(packageName);
-      this.#weekPacks.push(weekPack);
+      const weekPack = await this.getWeeklyDownloadsFromYesterday(packageName);
+      this.weekPacks.push(weekPack);
     }
   }
 
-  async start() {
-    await this.#getWeekPacks();
-    OutputView.printWeekPackageName(this.#weekPacks);
+  public async start() {
+    await this.getWeekPacks();
+    OutputView.printWeekPackageName(this.weekPacks);
     console.log("\nWeekly Total Downloads for All Packages:");
   }
 }
