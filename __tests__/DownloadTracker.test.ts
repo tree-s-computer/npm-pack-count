d375c097107cc3227b5170e28c0c1c5eb4d12459
Lukas.J.Han <lukas.j.han@gmail.com>
2024-03-17 21:23:31 +0900

    feat: add typscript and vitest

--- __tests__/DownloadTracker.test.ts
+++ __tests__/DownloadTracker.test.ts
@@ -1,8 +1,11 @@
+import { describe, expect, test } from 'vitest';
+
 function sum(a, b) {
   return a + b;
 }
-describe("sum module", () => {
-  test("adds 1 + 2 to equal 3", () => {
+
+describe('sum module', () => {
+  test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
   });
 });
