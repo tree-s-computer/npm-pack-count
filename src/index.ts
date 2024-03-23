4f12c9ef2071698e0690cffa2bc7dfa2ed0eb75b
tree <hhyjs2r@gmail.com>
2024-03-23 14:41:11 +0900

    feat: update version 0.0.3

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import { WEEK_NUM, PACKAGES } from './data';
 import DownloadTracker from './controllers/DownloadTrackerController';
 
-export default DownloadTracker
+export default DownloadTracker;
