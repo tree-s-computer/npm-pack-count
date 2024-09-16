311be93f028a7a814031e59000b527143c0a3d17
tree <hhyjs2r@gmail.com>
2024-09-17 01:21:32 +0900

    chore: add license mit

--- __tests__/formatDate.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, test } from 'vitest';
-import { formatDate } from '../src/util';
-
-describe('Test Format Date', () => {
-  test('Test#1', () => {
-    expect(formatDate(new Date('2024-03-17T10:03:23'))).toBe('2024-03-17');
-  });
-});
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 import DownloadTracker from './controllers/DownloadTrackerController';
-import { PackagesType, AllPackagesType } from './type/PackageType';
 
 export default DownloadTracker;
